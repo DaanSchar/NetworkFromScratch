25646609a9848a694f49b12a8204cd871b7a5599
DaanSchar <Daan.Schaer@Gmail.com>
2021-12-29 04:26:35 +0100

    major network refactor

--- src/main/java/network/neural/NeuralNetwork.java
+++ src/main/java/network/neural/NeuralNetwork.java
@@ -1,6 +1,7 @@
 package network.neural;
 
 import me.tongfei.progressbar.ProgressBar;
+import network.neural.activationfunctions.IActivationFunction;
 import network.neural.charts.LineChart;
 import network.neural.layer.Layer;
 import network.neural.layer.LayerOutput;
@@ -9,8 +10,8 @@ import network.neural.util.matrix.NDArray;
 import network.neural.util.readwrite.ObjectIO;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * A multi-layer, fully connected, artificial neural network.
@@ -23,7 +24,6 @@ public class NeuralNetwork implements Serializable {
     private double learningRate;
     private boolean costGraph;
 
-    private DataSet trainingSet;
     private DataSet[] batchList;
     private int m;
 
@@ -84,7 +84,6 @@ public class NeuralNetwork implements Serializable {
      * @param epochs the number of epochs to train for
      */
     public void train(DataSet trainingSet, int epochs, int batchSize) {
-        this.trainingSet = trainingSet;
         this.batchList = DataSet.batch(trainingSet, batchSize);
         this.m = batchList.length * batchSize;
 
@@ -99,85 +98,28 @@ public class NeuralNetwork implements Serializable {
      * @return the cost for each epoch
      */
     private double[] train(int epochs) {
-        double[] costs = new double[epochs];
-        long start = System.currentTimeMillis();
 
-//        try (ProgressBar pb = new ProgressBar("Training", epochs)) {
-//            for (int i = 0; i < epochs; i++) {
-//                for (int j = 0; j < batchList.length; j++) {
-//                    costs[i] += backpropagation(j);
-//                }
-//                pb.step();
-//            }
-//        }
+        double[] costs; // list of costs for each epoch
+        double totalTime; // total time of training
+
         try (ProgressBar pb = new ProgressBar("Training", epochs)) {
+            long startTime = System.currentTimeMillis();
+            costs = new double[epochs];
+
+
             for (int epoch = 0; epoch < epochs; epoch++) {
+                for (int batch = 0; batch < batchList.length; batch++)
+                    costs[epoch] += backpropagation(batch);
 
-                BackPropThread[] threads = new BackPropThread[batchList.length];
 
-                for (int i = 0; i < batchList.length; i++) {
-                    threads[i] = new BackPropThread(i);
-                    threads[i].start();
-                }
-
-                try {
-                    for (BackPropThread thread : threads)
-                        thread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                NDArray[] errors = threads[0].getErrors();
-
-                for (int i = 0; i < errors.length; i++) {
-                    for (int j = 1; j < threads.length; j++) {
-                        errors[i] = errors[i].add(threads[j].getErrors()[i]);
-                    }
-                    layers.get(i).setError(errors[i].mul(1.0 / (double) batchList.length));
-                }
-
-//                updateWeights(forward(batchList[new Random().nextInt(batchList.length-1)].getX()));
-
-                for (int i = 0; i < batchList.length; i++) {
-                    ArrayList<LayerOutput> outputs = forward(batchList[i].getX());
-                    updateWeights(forward(batchList[i].getX()));
-                    double cost = cost(batchList[i].getY(), outputs.get(outputs.size() - 1).getA()).sum();
-                    costs[epoch] += cost;
-                }
                 pb.step();
+                pb.setExtraMessage("Cost: " + new DecimalFormat("#.##").format(costs[epoch]));
             }
+            totalTime = (System.currentTimeMillis() - startTime) / 1000.0;
+            System.out.println("\nTraining complete in " + totalTime + " seconds.");
         }
-        long end = System.currentTimeMillis();
-        System.out.println("Time: " + (end - start) / 1000.0);
 
         return costs;
-    }
-
-    private double getCost() {
-        double cost = 0;
-        for (int i = 0; i < batchList.length; i++) {
-            ArrayList<LayerOutput> outputs = forward(batchList[i].getX());
-            cost += cost(batchList[i].getY(), outputs.get(outputs.size() - 1).getA()).sum();
-        }
-        return cost;
-    }
-
-    class BackPropThread extends Thread {
-        private NDArray[] errors;
-        private int batch;
-
-        public BackPropThread(int batch) {
-            this.batch = batch;
-        }
-
-        @Override
-        public void run() {
-            errors = backpropagations(batch);
-        }
-
-        public NDArray[] getErrors() {
-            return errors;
-        }
     }
 
 
@@ -220,28 +162,32 @@ public class NeuralNetwork implements Serializable {
 
 
     /**
-     * performs one time backward propagation
+     * performs a single run of backward propagation on a batch
      *
      * @return the cost
      */
     private double backpropagation(int batchIndex) {
-        ArrayList<LayerOutput> outputs = forward(batchList[batchIndex].getX());
 
-        NDArray[] errors = getErrors(outputs, batchIndex);
+        ArrayList<LayerOutput> outputs; // the outputs of each layer.
+        DataSet batch;                  // the batch of data.
+        NDArray[] errors;               // the errors of each layer.
+        NDArray prediction;             // the output/prediction of the network.
+        double cost;                    // the cost of the batch.
 
-        for (int i = 0; i < layers.size(); i++) {
+        batch = batchList[batchIndex];
+        outputs = forward(batch.getX());
+        errors = getErrors(outputs, batchIndex);
+
+        // assign the computed errors to each layer.
+        for (int i = 0; i < layers.size(); i++)
             layers.get(i).setError(errors[i]);
-        }
 
         updateWeights(outputs);
 
-        return cost(batchList[batchIndex].getY(), outputs.get(outputs.size() - 1).getA()).sum();
-    }
+        prediction = outputs.get(outputs.size() - 1).getA();
+        cost = cost(batch.getY(), prediction).sum();
 
-    private NDArray[] backpropagations(int batchIndex) {
-        ArrayList<LayerOutput> outputs = forward(batchList[batchIndex].getX());
-
-        return getErrors(outputs, batchIndex);
+        return cost;
     }
 
 
@@ -254,17 +200,31 @@ public class NeuralNetwork implements Serializable {
      * @return array of errors of each layer
      */
     private NDArray[] getErrors(ArrayList<LayerOutput> outputs, int batchIndex) {
-        NDArray[] errors = new NDArray[layers.size()];
-        errors[layers.size()-1] = getFinalLayerError(outputs.get(outputs.size() - 1), batchIndex);
+        NDArray[] errors;          // the errors of each layer.
+        LayerOutput prediction;    // the output/prediction of the network.
+
+        errors = new NDArray[layers.size()];
+        prediction = outputs.get(outputs.size() - 1);
+        errors[layers.size()-1] = getFinalLayerError(prediction, batchIndex);
+
+        Layer currentLayer;
+        Layer nextLayer;
+        NDArray error;
+        NDArray weights;            // the weights of the next layer.
+        IActivationFunction actFun; // the activation function of the current layer.
+        NDArray z;                  // output of the next layer
 
         for  (int i = layers.size()-2; i >= 0; i--) {
-            Layer currentLayer = layers.get(i);
-            Layer nextLayer = layers.get(i + 1);
+            currentLayer = layers.get(i);
+            nextLayer = layers.get(i + 1);
+            weights = nextLayer.getWeights();
 
-            NDArray error = nextLayer
-                    .getWeights().T()
+            z = outputs.get(i+1).getZ();
+            actFun = currentLayer.getActivationFunction();
+
+            error = weights.T()
                     .dot(errors[i+1])
-                    .mul(outputs.get(i+1).getZ().gradient(currentLayer.getActivationFunction()).T());
+                    .mul(z.gradient(actFun).T());
 
             errors[i] = error;
         }
@@ -274,34 +234,62 @@ public class NeuralNetwork implements Serializable {
 
     /**
      * Computes the error of the final layer.
-     * error of a FinalLayer L = derivative(cost) * activation'(z^L)
+     * error of a FinalLayer L = cost' * activation'(z^L)
      *
      * @param lastOutput the output of the last layer
      * @param batchIndex the index of the batch
      * @return the error of the final layer
      */
     private NDArray getFinalLayerError(LayerOutput lastOutput, int batchIndex) {
-        Layer finalLayer = layers.get(layers.size() - 1);
-        return costDerivative(batchList[batchIndex].getY(), lastOutput.getA()).mul(lastOutput.getZ().gradient(finalLayer.getActivationFunction())).T();
+
+        Layer finalLayer;                       // the final layer of our network.
+        NDArray costDerivative;                 // derivative of the cost function at the current state of the network.
+        IActivationFunction activationFunction; // activation function of the last layer (Sigmoid, Relu, etc).
+        NDArray z;                              // output of the last layer.
+
+
+        finalLayer = layers.get(layers.size() - 1);
+        costDerivative = costDerivative(batchList[batchIndex].getY(), lastOutput.getA());
+        activationFunction = finalLayer.getActivationFunction();
+        z = lastOutput.getZ();
+
+        return costDerivative.mul(z.gradient(activationFunction)).T();
     }
 
 
     /**
-     * updates the weights of each layer
+     * updates the weights and bias of each layer
      * using the error of the layer computed in updateLayerError().
-     * weights^l += error^l dot activation^l-1
+     * weights^l += error^l dot activation^l-1.
+     * bias^l += error^l.
      *
      * @param outputs list of forward propagation outputs of all the layers layer
      */
     private void updateWeights(ArrayList<LayerOutput> outputs) {
+
+        Layer layer;            // the current layer.
+        NDArray weight;         // weight of the layer.
+        NDArray bias;           // bias of the layer.
+
+        NDArray error;          // error of the layer.
+        NDArray a;              // activation of the layer.
+
+        NDArray updatedWeight;  // the new weight.
+        NDArray updatedBias;    // the new bias.
+
         for (int i = 0; i < layers.size(); i++) {
-            Layer layer = layers.get(i);
+            layer = layers.get(i);
+            weight = layer.getWeights();
+            bias = layer.getBias();
 
-            NDArray newWeights = layer.getWeights().add(layer.getError().dot(outputs.get(i).getA()).mul(learningRate));
-            NDArray newBias = layer.getBias().add(layer.getError().mul(learningRate).getAvgColVector());
+            error = layer.getError();
+            a = outputs.get(i).getA();
 
-            layer.setWeights(newWeights);
-            layer.setBias(newBias);
+            updatedWeight = weight.add(error.dot(a).mul(learningRate));
+            updatedBias = bias.add(error.mul(learningRate).getAvgColVector());
+
+            layer.setWeights(updatedWeight);
+            layer.setBias(updatedBias);
         }
     }
 
@@ -310,29 +298,47 @@ public class NeuralNetwork implements Serializable {
      * does forward propagation on each layer of the network
      * and returns the output of each layer
      *
-     * @param input data we want to make a prediction on
+     * @param x data we want to make a prediction on
      * @return the prediction
      */
-    private ArrayList<LayerOutput> forward(NDArray input) {
-        ArrayList<LayerOutput> outputs = new ArrayList<>();
-        outputs.add(new LayerOutput(input, input));
+    private ArrayList<LayerOutput> forward(NDArray x) {
 
-        for (Layer layer : layers)
-            outputs.add(layer.forward(outputs.get(outputs.size() - 1).getA()));
+        ArrayList<LayerOutput> outputs; // collection of all the outputs of each layer.
+        LayerOutput input;             // the output of the input layer (l = 0). this layer does not contain weights.
+        LayerOutput lastOutput;        // the output of the last computed layer.
+        LayerOutput nextOutput;         // the output of the next layer.
+
+        outputs = new ArrayList<>();
+        input = new LayerOutput(x, x); // a and z are both the same for the input layer.
+        outputs.add(input);
+
+        for (Layer layer : layers) {
+            lastOutput = outputs.get(outputs.size() - 1);
+            nextOutput = layer.forward(lastOutput.getA());
+            outputs.add(nextOutput);
+        }
 
         return outputs;
     }
 
 
     /**
-     * Cost function.
+     * Cost function. (MSE)
      *
      * @param y labels
      * @param yHat predictions
      * @return error squared vector
      */
     private NDArray cost(NDArray y, NDArray yHat) {
-        return y.sub(yHat).pow(2).mul(1.0/(m*2.0));
+        NDArray error; // error between the labels and the predictions.
+        NDArray cost;  // the average cost of a training instance.
+        double m;               // the number of training instances.
+
+        m = this.m;
+        error = y.sub(yHat);
+        cost = error.pow(2).mul(1.0/(m*2.0));
+
+        return cost;
     }
 
 
@@ -344,7 +350,15 @@ public class NeuralNetwork implements Serializable {
      * @return error' vector
      */
     private NDArray costDerivative(NDArray y, NDArray yHat) {
-        return y.sub(yHat).mul(2.0).mul(1.0/m);
+        NDArray error;          // the error of the predictions.
+        NDArray costDerivative; // the derivative of the cost function.
+        double m;               // the number of training instances.
+
+        m = this.m;
+        error = y.sub(yHat);
+        costDerivative = error.mul(1.0/m);
+
+        return costDerivative;
     }
 
 
